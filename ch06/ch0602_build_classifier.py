fb0893621018ed8b9e932dbc98ab906cc6e34a75
peterhchen <peter.hp.chen@gmail.com>
2018-09-22 22:22:33 -0700

    Add files via upload

---
+++ ch06/ch0602_build_classifier.py
@@ -0,0 +1,44 @@
+# step 1: import Scikit-learn
+import sklearn
+import warnings
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+
+# step 2: import scikit-learn dataset. 
+# This load_breast_cancer is the database of breast cancer from Wisconsin
+from sklearn.datasets import load_breast_cancer
+# load the dataset
+data = load_breast_cancer()
+# Create variable for each dataset
+label_names = data['target_names']
+labels = data['target']
+feature_names = data['feature_names']
+features = data['data']
+
+# print the class label
+print('label_names: ' , label_names)
+print('label_names[0]: ' , label_names[0])
+print('label_names[1]: ' , label_names[1])
+# print label number
+print('labels[0]: ' , labels[0])
+print('labels[1]: ' , labels[1])
+# print feature name and values
+print('feature_names[0]: ' , feature_names[0])
+print('features[0]: ')
+print(features[0])
+
+# step 3: orginize data into set: import split function
+from sklearn.model_selection import train_test_split
+train, test, train_labels, test_labels = \
+train_test_split(features,labels,test_size = 0.40, random_state = 42)
+
+# step 4: build the model: Naive Bayes algorithm
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+model = gnb.fit(train, train_labels)
+
+# Step 5: evaluate the model and its accuracy
+preds = gnb.predict(test)
+print ('preds: ')
+print(preds)
+from sklearn.metrics import accuracy_score
+print ('accuracy_score: ', accuracy_score(test_labels,preds))
---
+++ ch06/ch0603_svm.py
@@ -0,0 +1,32 @@
+# SVM (Support Vector Machines) for regression and classification
+# pip3 install pandas
+import pandas as pd
+import numpy as np
+from sklearn import svm, datasets
+# pip3 install matplotlib
+# import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt 
+import warnings
+warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+# Get iris dataset
+iris = datasets.load_iris()
+X = iris.data[:, :2]
+#print ('X: ', X)
+y = iris.target
+x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+h = (x_max / x_min)/100
+xx, yy = np.meshgrid(np.arange(x_min, x_max, h),np.arange(y_min, y_max, h))
+X_plot = np.c_[xx.ravel(), yy.ravel()]
+C = 1.0
+svc_classifier = svm.SVC(kernel='linear', C=C, decision_function_shape = 'ovr').fit(X, y)
+Z = svc_classifier.predict(X_plot)
+Z = Z.reshape(xx.shape)
+plt.figure(figsize = (15, 5))
+plt.subplot(121)
+plt.contourf(xx, yy, Z, cmap = plt.cm.tab10, alpha = 0.3)
+plt.scatter(X[:, 0], X[:, 1], c = y, cmap = plt.cm.Set1)
+plt.xlabel('Sepal length')
+plt.ylabel('Sepal width')
+plt.xlim(xx.min(), xx.max()) 
+plt.show()
